b2567ebcd4deec873cf97ae544cc817632b7b80c
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-28 05:36:51 -0300

    Update message server

--- message_server.py
+++ message_server.py
@@ -2,21 +2,24 @@ from flask import Flask
 import datetime
 
 
-talk = []
-sep = '-$$-$$-'
+SEP = '-$$-$$-'
+LINE_B = '-@@-@@-'
+
+line = '2019-06-27 23:48:47.678' + SEP + 'carl' + SEP + 'this is a message'
+talk = [line, line, line]
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '\n'.join(talk)
+    return LINE_B.join(talk)
 
 
 @app.route('/user/<user>/message/<message>')
 def show_post(user, message):
     time = str(datetime.datetime.now())
-    talk.append(sep.join([time, user, message]))
-    return '\n'.join(talk)
+    talk.append(SEP.join([time, user, message]))
+    return LINE_B.join(talk)
 
 
 app.run()
