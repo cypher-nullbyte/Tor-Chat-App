829755fc0928f4a61cd08ae8362fccece8709763
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-28 05:58:15 -0300

    Work chat

--- message_server.py
+++ message_server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import datetime
+import urllib.parse
 
 
 SEP = '-$$-$$-'
@@ -17,6 +18,8 @@ def index():
 
 @app.route('/user/<user>/message/<message>')
 def show_post(user, message):
+    user = urllib.parse.unquote(user)
+    message = urllib.parse.unquote(message)
     time = str(datetime.datetime.now())
     talk.append(SEP.join([time, user, message]))
     return LINE_B.join(talk)
