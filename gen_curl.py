996b73604f6dd801300759379f38373c74d80315
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-28 01:24:24 -0300

    Transform into class

--- gen_curl.py
+++ gen_curl.py
@@ -1,32 +1,32 @@
 import io
 import pycurl
-from stem.util import term
 
 
-def make_query(url):
-    """
-    Uses pycurl to fetch a site using the proxy on the socks_port
-    :param url: the string url
-    :return: the website string
-    """
+class TorQuery:
+    def __init__(self, domain, socks_port):
+        self.domain = domain
+        self.socks_port = socks_port
 
-    output = io.BytesIO()
+    def query(self, route):
+        """
+        Uses pycurl to fetch a site using the proxy on the socks_port
+        :param url: the string url
+        :param route: the route in the website
+        :return: the website string
+        """
 
-    query = pycurl.Curl()
-    query.setopt(pycurl.URL, url)
-    query.setopt(pycurl.PROXY, 'localhost')
-    query.setopt(pycurl.PROXYPORT, SOCKS_PORT)
-    query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
-    query.setopt(pycurl.WRITEFUNCTION, output.write)
+        domain = self.domain + route
+        output = io.BytesIO()
 
-    try:
-        query.perform()
-        return output.getvalue()
-    except pycurl.error as exc:
-        return "Unable to reach %s (%s)" % (url, exc)
+        query = pycurl.Curl()
+        query.setopt(pycurl.URL, domain)
+        query.setopt(pycurl.PROXY, 'localhost')
+        query.setopt(pycurl.PROXYPORT, self.socks_port)
+        query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
+        query.setopt(pycurl.WRITEFUNCTION, output.write)
 
-
-SOCKS_PORT = int(input("What is the tor SOCKS_PORT? "))
-website = input("What is the website? ")
-
-print(term.format(make_query(website), term.Color.BLUE))
+        try:
+            query.perform()
+            return output.getvalue()
+        except pycurl.error as exc:
+            return "Unable to reach %s (%s)" % (domain, exc)
