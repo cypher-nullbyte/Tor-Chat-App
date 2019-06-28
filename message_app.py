7dfef584bef292e625f9369dc3daaa6a57c1b8ed
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-28 03:25:49 -0300

    Try to async

--- message_app.py
+++ message_app.py
@@ -1,22 +1,54 @@
 import datetime
 import PySimpleGUI as sg
 from tor_query import TorQuery
+import asyncio
 
 
 SEP = '-$$-$$-'
+LINE_B = '-@@-@@-'
 
 
+def update_talk(talk):
+    talk_to_print = []
+    for line in talk:
+        if line:
+            time, user, message = line.split(SEP)
+            time = time.split('.')[0]
+            time = time[:-3]
+            time = ''.join(time)
+            talk_to_print.append('[%s] %s: %s' % (time, user, message))
+    window.Element('_OUTPUT_').Update('\n'.join(talk_to_print))
+
+
+@asyncio.coroutine
 def send():
-    print(values['_IN_'])
+    # Update text
+
+    # time = str(datetime.datetime.now())
+    # talk.append(SEP.join([time, user, message]))
+    # update_talk(talk)
+
+    # Query post message
+
     message = values['_IN_']
-    time = str(datetime.datetime.now())
-    talk.append(SEP.join([time, user, message]))
-    window.Element('_OUTPUT_').Update(values['_IN_'])
+    route = 'user/%s/message/%s' % (user, message)
+    talk_string = tor_query.query(route)
+    print(talk_string)
+    talk = talk_string.split(LINE_B)
+    update_talk(talk)
+
+    # return talk
 
 
+@asyncio.coroutine
 def update():
     print('update')
-    window.Element('_OUTPUT_').Update(values['_IN_'])
+    talk_string = tor_query.query()
+    print(talk_string)
+    talk = talk_string.split(LINE_B)
+    update_talk(talk)
+
+    # return talk
 
 
 if __name__ == '__main__':
@@ -36,14 +68,18 @@ if __name__ == '__main__':
     ]
 
     window = sg.Window('Tor Chat', layout)
-
+    loop = asyncio.get_event_loop()
     while True:
         event, values = window.Read(timeout=500)
-        print(event, values)
+        # print(event, values)
         if event == 'Send':
             send()
         elif event == '__TIMEOUT__':
-            update()
+            try:
+                # update().send(datetime.datetime.now())
+                loop.run_until_complete(update())
+            except StopIteration as exe:
+                print('executed %s' % exe)
         elif event is None or event == 'Exit':
             break
 
